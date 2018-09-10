215625da9bf54bf9137779c0eaea7d682b501db2
frank8248 <42793306+frank8248@users.noreply.github.com>
2018-09-10 11:59:39 +0800

    Add files via upload

---
+++ p5jsTutorial1.1-master/sketch.js
@@ -0,0 +1,53 @@
+function setup()
+{
+  //create a drawing service 700px wide, 500px tall
+  createCanvas(801,801);
+}
+
+function draw()
+{
+  //create a background using RGB values
+  background(0,0,0)
+
+  // change the paint brush to a specific color according to RGB values
+  stroke(0,0,0)
+
+  // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
+  fill(255,255,255);
+  rect(0,0,100,100);
+  rect(0,200,100,100);
+  rect(0,400,100,100);
+  rect(0,600,100,100);
+  rect(200,0,100,100);
+  rect(400,0,100,100);
+  rect(600,0,100,100);
+  rect(100,100,100,100);
+  rect(300,100,100,100);
+  rect(500,100,100,100);
+  rect(700,100,100,100);
+  rect(200,200,100,100);
+  rect(200,400,100,100);
+  rect(200,600,100,100);
+  rect(400,200,100,100);
+  rect(600,200,100,100);
+  rect(100,300,100,100);
+  rect(300,300,100,100);
+  rect(500,300,100,100);
+  rect(700,300,100,100);
+  rect(100,500,100,100);
+  rect(300,500,100,100);
+  rect(500,500,100,100);
+  rect(700,500,100,100);
+  rect(100,700,100,100);
+  rect(300,700,100,100);
+  rect(500,700,100,100);
+  rect(700,700,100,100);
+  rect(400,400,100,100);
+  rect(600,400,100,100);
+  rect(800-400,800-200,100,100);
+  rect(600,600,100,100);
+  _
+
+
+    
+}
